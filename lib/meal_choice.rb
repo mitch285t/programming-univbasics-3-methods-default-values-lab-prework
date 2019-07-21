390996669c5edb321c4d88cb35184c8ebd5400fe
mitchell williams <mscottwilliams1994@gmail.com>
2019-07-21 19:38:23 +0000

    Automatically backed up by Learn

--- lib/meal_choice.rb
+++ lib/meal_choice.rb
@@ -1,4 +1,7 @@
 meal_choice(veg1,veg2,protein = "meat")
 puts "What a nutritious meal!"
-puts "A plate of #{protein} with #{veg1} and #{veg2}."
+
+return puts "A plate of #{protein} with #{veg1} and #{veg2}."
+
+
 end
